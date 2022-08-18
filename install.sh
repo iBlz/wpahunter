5a56bf29681a526ac73824c896abc876a1f7db8d
iBlaze <58302540+iBlz@users.noreply.github.com>
2022-08-18 14:04:51 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+apt update && apt upgrade -y
+apt install aircrack-ng hcxdumptool hcxtools tcpdump tshark reaver netdiscover python3-pip python3-pandas -y
+pip3 install simple_term_menu tabulate colorama
+echo 
+echo Done!
