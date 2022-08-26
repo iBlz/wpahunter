5451a632bc84a69df377b514519ee2778569f2f9
iBlaze <58302540+iBlz@users.noreply.github.com>
2022-08-26 22:32:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt update && apt upgrade -y
-apt install aircrack-ng hcxdumptool hcxtools tcpdump tshark reaver netdiscover python3-pip python3-pandas jq -y
+apt install aircrack-ng hcxdumptool hcxtools tcpdump tshark reaver netdiscover python3-pip python3-pandas jq mdk3 -y
 pip3 install simple_term_menu tabulate colorama
 echo 
 echo Done!
